a54d0f66c23368aaf7285c830dac1db962067c51
Matt Sears <mattsears@engr2-1-13-122-edu.int.colorado.edu>
2017-02-23 20:59:00 -0700

    read journal JSON file in prep to scrape all journals

--- api/controllers/ScraperController.js
+++ api/controllers/ScraperController.js
@@ -5,14 +5,13 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var $ = require('cheerio');
-var app     = express();
 
 module.exports = {
 	scrape: function(req, res) {
+		var journals = JSON.parse(fs.readFileSync('./scraper/data/journals.json', 'utf8'));
 
 		url = 'http://ascelibrary.org/journal/jcemd4';
 		cookie = "I2KBRCK=1; JSESSIONID=aaa85T3KL8GE5cmcdoRPv; MACHINE_LAST_SEEN=2017-02-23T17%3A55%3A06.481-08%3A00; MAID=AvStR0Q1F/ZiaXXY5v1L3g==; SERVER=WZ6myaEXBLEcZv1pIhVX+g==; _dc_gtm_UA-56132535-29=1; _dc_gtm_UA-8940040-23=1; _ga=GA1.2.1187409641.1487820890; _hjIncludedInSample=1";
@@ -46,25 +45,13 @@ module.exports = {
 					all_issues.push(issue_data);
 				});
 
-				res.send(html);
+				//res.send(html);
+				res.send(journals);
 			}
 
-			// To write to the system we will use the built in 'fs' library.
-			// In this example we will pass 3 parameters to the writeFile function
-			// Parameter 1 :  output.json - this is what the created filename will be called
-			// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-			// Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-			fs.writeFile('./data/issues.json', JSON.stringify(all_issues, null, 4), function(err){
-
+			fs.writeFile('./scraper/data/issues.json', JSON.stringify(all_issues, null, 4), function(err){
 			    console.log('File successfully written! - Check your project directory for the output.json file');
-
 			});
-
-			// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-			//res.send('Check your console!');
-
 		});
-
 	}
 };
